eee0bd3ff916cce96004e28a0438528eeb4f66b2
Aebel Shajan <aebelshajan@gmail.com>
2024-02-09 12:55:38 +0000

    added box object to world

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
 import * as CANNON from 'cannon-es';
 
+
+// Setup
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
@@ -14,14 +16,36 @@ const camera = new THREE.PerspectiveCamera(
     1000
 );
 
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.81, 0)
+});
+
 // Sets orbit control to move the camera around
 const orbit = new OrbitControls(camera, renderer.domElement);
 
 // Camera positioning
-camera.position.set(20, 0, 20);
+camera.position.set(30, 30, 30);
 orbit.update();
 
-const groundGeo = new THREE.PlaneGeometry(10, 10);
+
+
+// Objects
+const boxGeo = new THREE.BoxGeometry(2, 2, 2);
+const boxMat = new THREE.MeshBasicMaterial({
+    color: 0x00ff00,
+    wireframe: true
+});
+const boxMesh = new THREE.Mesh(boxGeo, boxMat);
+scene.add(boxMesh);
+
+const boxBody = new CANNON.Body({
+    shape: new CANNON.Box(new CANNON.Vec3(1, 1, 1)),
+    mass: 1,
+    position: new CANNON.Vec3(0, 10, 0),
+});
+world.addBody(boxBody);
+
+const groundGeo = new THREE.PlaneGeometry(30, 30);
 const groundMat = new THREE.MeshBasicMaterial({
     color: 0xffffff,
     side: THREE.DoubleSide,
@@ -30,14 +54,27 @@ const groundMat = new THREE.MeshBasicMaterial({
 const groundMesh = new THREE.Mesh(groundGeo, groundMat);
 scene.add(groundMesh);
 
-const world = new CANNON.World({
-    gravity: new CANNON.Vec3(0, -9.81, 0)
-});
+const groundBody = new CANNON.Body({
+    shape: new CANNON.Plane(),
+    // mass: 10,
+    type: CANNON.Body.STATIC
+})
+groundBody.quaternion.setFromEuler(- Math.PI / 2, 0, 0);
+world.addBody(groundBody)
+
 
 const timeStep = 1 / 60;
 
 function animate() {
-    world.step(timeStep)
+    world.step(timeStep);
+
+    boxMesh.position.copy(boxBody.position);
+    boxMesh.quaternion.copy(boxBody.quaternion);
+
+    groundMesh.position.copy(groundBody.position);
+    groundMesh.quaternion.copy(groundBody.quaternion);
+
+
     renderer.render(scene, camera);
 }
 
