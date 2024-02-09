3657977ea54417c770576ca8a1399124bccb8e46
Aebel Shajan <aebelshajan@gmail.com>
2024-02-09 11:46:06 +0000

    created plane and initialised cannon physics world

--- src/index.js
+++ src/index.js
@@ -1,12 +1,10 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as CANNON from 'cannon-es';
 
 const renderer = new THREE.WebGLRenderer({antialias: true});
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-// Sets the color of the background
-renderer.setClearColor(0xFEFEFE);
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(
@@ -20,18 +18,26 @@ const camera = new THREE.PerspectiveCamera(
 const orbit = new OrbitControls(camera, renderer.domElement);
 
 // Camera positioning
-camera.position.set(6, 8, 14);
+camera.position.set(20, 0, 20);
 orbit.update();
 
-// Sets a 12 by 12 gird helper
-const gridHelper = new THREE.GridHelper(12, 12);
-scene.add(gridHelper);
+const groundGeo = new THREE.PlaneGeometry(30, 30);
+const groundMat = new THREE.MeshBasicMaterial({
+    color: 0xffffff,
+    side: THREE.DoubleSide,
+    wireframe: true
+})
+const groundMesh = new THREE.Mesh(groundGeo, groundMat);
+scene.add(groundMesh);
 
-// Sets the x, y, and z axes with each having a length of 4
-const axesHelper = new THREE.AxesHelper(4);
-scene.add(axesHelper);
+const world = new CANNON.World({
+    gravity: new CANNON.Vec3(0, -9.81, 0)
+});
+
+const timeStep = 1 / 60;
 
 function animate() {
+    world.step(timeStep)
     renderer.render(scene, camera);
 }
 
