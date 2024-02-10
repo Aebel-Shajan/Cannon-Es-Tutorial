5a3b539cd931937155cba8736d6baf806c83f252
Aebel Shajan <aebelshajan@gmail.com>
2024-02-10 14:02:38 +0000

    Remove wireframe from ground material

--- src/index.js
+++ src/index.js
@@ -64,7 +64,6 @@ const groundGeo = new THREE.PlaneGeometry(30, 30);
 const groundMat = new THREE.MeshBasicMaterial({
     color: 0xffffff,
     side: THREE.DoubleSide,
-    wireframe: true
 })
 const groundMesh = new THREE.Mesh(groundGeo, groundMat);
 scene.add(groundMesh);
